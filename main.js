cc6985ad21d90792cc6f791b51460ca6e1955a1c
abdullah <abdullahaboalm2004@gmail.com>
2024-02-08 10:20:50 +0200

    updates

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-const myEmail = document.querySelector('#email');
-const myBtn = document.querySelector('#btn');
-const myErr = document.querySelector('#Err');
+const myEmail = document.querySelector("#email");
+const myBtn = document.querySelector("#btn");
+const myErr = document.querySelector("#Err");
 
-myBtn.addEventListener('click', () => {
-  let Text = myEmail.value.split('');
+myBtn.addEventListener("click", () => {
+  let Text = myEmail.value.split("");
   if (Text.length === 0) {
-    myErr.style.cssText = 'display: block';
-    myEmail.style.cssText = 'border-color: #f56273; outline: none';
-  } else if (Text.indexOf('@') === -1 || Text.indexOf('.') === -1 || Text[Text.indexOf('@') - 1] === '.' || Text[Text.indexOf('@') + 1] === '.' || Text[Text.indexOf('@') - 1] === undefined || Text[Text.indexOf('.') + 1] === undefined) {
-    myErr.style.cssText = 'display: block';
-    myEmail.style.cssText = 'border-color: #f56273; outline: none';
-  } else {
-    myErr.style.cssText = 'display: block; color: rgb(0, 132, 255);';
-    myErr.textContent = 'Thanks !';
+    myErr.style.cssText = "display: block";
+    myEmail.style.cssText = "border-color: #f56273; outline: none";
+  } else if (
+    /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,3}$/.test(myEmail.value)
+  ) {
+    myErr.style.cssText = "display: block; color: rgb(0, 132, 255);";
+    myErr.textContent = "Thanks !";
     setTimeout(() => {
       location.reload();
     }, 1000);
+  } else {
+    myErr.style.cssText = "display: block";
+    myEmail.style.cssText = "border-color: #f56273; outline: none";
   }
-})
+});
 
-myEmail.addEventListener('input', () => {
-  myErr.style.cssText = 'display: none';
-  myEmail.style.cssText = 'border-color: #cfcfcf';
-})
+myEmail.addEventListener("input", () => {
+  myErr.style.cssText = "display: none";
+  myEmail.style.cssText = "border-color: #cfcfcf";
+});
